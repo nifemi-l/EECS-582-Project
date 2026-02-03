0ef7fb9af7fc56ae3e098291bea0fa88a376b69f
Dellie <delsterone@gmail.com>
2026-02-03 16:31:05 -0600

    create boilerplate for tasks, tasklocations, households

---
+++ src/server/household.py
@@ -0,0 +1,12 @@
+from task_location import TaskLocation
+from task import Task 
+
+class Household:
+    def __init__(self, user : str):
+        self.task_locations = []
+
+    def add_task_location(task_location):
+        assert(self == task_location.household)
+        self.task_locations.append(task_location)
+
+
---
+++ src/server/task.py
@@ -0,0 +1,8 @@
+from task_location import TaskLocation
+
+class Task:
+    def __init__(self, location : TaskLocation, taskString : str):
+        self.location = location
+        self.taskString = taskString
+
+    
