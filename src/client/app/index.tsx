a966852bd04d2cbd7bc622cdf2876707e31b6b5a
Jack Bauer <jgb1017@gmail.com>
2026-02-15 14:03:28 -0600

    Now draws a grid line with support for both WebGL 1.0 and 2.0

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -26,11 +26,14 @@ export default function Index() {
 let glRef: ExpoWebGLRenderingContext | null = null;
 let shaderProgram: WebGLProgram | null = null;
 let lastFrameTime = 0;
+let oesExt: OES_vertex_array_object | null = null;
 
 class Household {
    blockVertices: Float32Array;
    modelMatrices: GLM.mat4[];
    modelLoc: WebGLUniformLocation | null;
+   buffer: WebGLBuffer | null;
+   vao: WebGLVertexArrayObject | WebGLVertexArrayObjectOES | null;
 
    constructor() {
     // Vertices + normal vectors o a cube
@@ -80,12 +83,41 @@ class Household {
 
     this.modelMatrices = [GLM.mat4.create(), GLM.mat4.create(), GLM.mat4.create(), GLM.mat4.create()];
     this.modelLoc = null;
+    this.buffer = null;
+    this.vao = null;
    }
 }
 const house = new Household();
 
+class Grid {
+  gridVertices: Float32Array;
+  modelMatrx: GLM.mat4;
+  buffer: WebGLBuffer | null;
+  vao: WebGLVertexArrayObject | WebGLVertexArrayObjectOES | null;
+
+  constructor() {
+    this.gridVertices = new Float32Array([
+      -100.0, 0.0, 0.0,
+      100.0, 0.0, 0.0,
+    ]);
+    
+    this.modelMatrx = GLM.mat4.create();
+    this.buffer = null;
+    this.vao = null;
+  }
+}
+const grid = new Grid();
+
 async function onContextCreate(gl: ExpoWebGLRenderingContext) {
   const [vertData, fragData] = await readShaderData();
+
+  // Get the OES Vertex Array Object extension
+  // This is needed because these VAOs provide very useful functionality (we don't have to define vertex array attributes
+  // every frame). However, since we need to support WebGL 1.0 (for older Raspberry Pis), we need to pull this in as an extension
+  // as this functionality is only native in WebGL 2.0. To make things more annoying, often this functionality is NOT available in WebGL 2.0 
+  // contexts. So, it's stupid, but we have to support both. This getExtension(...) call will either return an object or null.
+  oesExt = gl.getExtension('OES_vertex_array_object'); 
+  console.log("OES VAO Extension available:", oesExt);
 
   // Reset everything so it works when navigating back to this page
   glRef = gl;
@@ -171,27 +203,47 @@ async function onContextCreate(gl: ExpoWebGLRenderingContext) {
   house.modelLoc = matrixUniformLocs.modelMatrix;
 
   // Setup our vertex buffer and attribute informations. This is how we know what information is stored where
-  const houseBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, houseBuffer);
+  house.buffer = gl.createBuffer();
+  house.vao = createVAO();
+  bindVAO(house.vao);
+  gl.bindBuffer(gl.ARRAY_BUFFER, house.buffer);
   gl.bufferData(gl.ARRAY_BUFFER, house.blockVertices, gl.STATIC_DRAW);
   gl.vertexAttribPointer(attribLocs.vertLoc, 3, gl.FLOAT, false, 6 * 4, 0); // using VAO 0 by default
   gl.enableVertexAttribArray(attribLocs.vertLoc);
   gl.vertexAttribPointer(attribLocs.normalLoc, 3, gl.FLOAT, false, 6 * 4, 4 * 3); // 4 bytes per float * 6 floats stored per vertex = 24 bytes per vertex
   gl.enableVertexAttribArray(attribLocs.normalLoc);  
+  bindVAO(null);
+
+  // Grid vertices
+  grid.buffer = gl.createBuffer();
+  grid.vao = createVAO();
+  bindVAO(grid.vao);
+  gl.bindBuffer(gl.ARRAY_BUFFER, grid.buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, grid.gridVertices, gl.STATIC_DRAW); 
+  gl.vertexAttribPointer(attribLocs.vertLoc, 3, gl.FLOAT, false, 3 * 4, 0);
+  gl.enableVertexAttribArray(attribLocs.vertLoc);
+  gl.disableVertexAttribArray(attribLocs.normalLoc);
+  gl.vertexAttrib3f(attribLocs.normalLoc, 0, 1, 0);
+  bindVAO(null);
+
+  // Select our shader program
   gl.useProgram(program);
 
   // Create our perspective matrix
   const projectionMatrix = GLM.mat4.create();
   const viewMatrix = GLM.mat4.create();
   GLM.mat4.perspective(projectionMatrix, (45 * Math.PI / 180), gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 100.0);
-  GLM.mat4.translate(house.modelMatrices[0], house.modelMatrices[0], [1.0, -1.0, -5.0]);
-  GLM.mat4.rotateY(house.modelMatrices[0], house.modelMatrices[0], 15);
-  GLM.mat4.translate(house.modelMatrices[1], house.modelMatrices[1], [-1.0, -1.0, -5.0]);
-  GLM.mat4.translate(house.modelMatrices[2], house.modelMatrices[2], [1.0, 1.0, -5.0]);
-  GLM.mat4.translate(house.modelMatrices[3], house.modelMatrices[3], [-1.0, 1.0, -5.0]);
+  GLM.mat4.translate(house.modelMatrices[0], house.modelMatrices[0], [0.0, 0.0, -5.0]);
+  // GLM.mat4.rotateY(house.modelMatrices[0], house.modelMatrices[0], 15);
+  // GLM.mat4.translate(house.modelMatrices[1], house.modelMatrices[1], [-1.0, -1.0, -5.0]);
+  // GLM.mat4.translate(house.modelMatrices[2], house.modelMatrices[2], [1.0, 1.0, -5.0]);
+  // GLM.mat4.translate(house.modelMatrices[3], house.modelMatrices[3], [-1.0, 1.0, -5.0]);
   gl.uniformMatrix4fv(matrixUniformLocs.projectionMatrix, false, projectionMatrix as Float32Array);
   gl.uniformMatrix4fv(matrixUniformLocs.modelMatrix, false, house.modelMatrices[0] as Float32Array);
   gl.uniformMatrix4fv(matrixUniformLocs.viewMatrix, false, viewMatrix as Float32Array);
+
+  // Move the grid lines back
+  GLM.mat4.translate(grid.modelMatrx, grid.modelMatrx, [0.0, 0.0, -5.0]);
 
   // Setup lighting
   gl.uniform3fv(lightUniformLocs.viewPosition, [0, 0, 0]);
@@ -232,6 +284,18 @@ function drawFrame(time: number) {
       return;
     }
 
+    // Ensure we have proper house and grid buffers
+    if (!house.buffer || !grid.buffer) {
+      console.error("Invalid buffers.");
+      return;
+    }
+
+    // Ensure we have proper house and grid vertex array objects (VAOs)
+    if (!house.vao || !grid.vao) {
+      console.error("Invalid VAOs.");
+      return;
+    }
+
     // Check time
     const delta = (time - lastFrameTime) / 1000;
     lastFrameTime = time;
@@ -239,30 +303,70 @@ function drawFrame(time: number) {
     // Prepare draw
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    // Cube draw calls
+    bindVAO(house.vao);
+
     // Cube 1
     GLM.mat4.rotateY(house.modelMatrices[0], house.modelMatrices[0], delta);
     gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[0] as Float32Array);
     gl.drawArrays(gl.TRIANGLES, 0, 36);
 
-    // Cube 2
-    GLM.mat4.rotateX(house.modelMatrices[1], house.modelMatrices[1], delta * 2);
-    gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[1] as Float32Array);
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
+    // // Cube 2
+    // GLM.mat4.rotateX(house.modelMatrices[1], house.modelMatrices[1], delta * 2);
+    // gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[1] as Float32Array);
+    // gl.drawArrays(gl.TRIANGLES, 0, 36);
 
-    // Cube 3
-    GLM.mat4.rotateZ(house.modelMatrices[2], house.modelMatrices[2], delta * 3);
-    gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[2] as Float32Array);
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
+    // // Cube 3
+    // GLM.mat4.rotateZ(house.modelMatrices[2], house.modelMatrices[2], delta * 3);
+    // gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[2] as Float32Array);
+    // gl.drawArrays(gl.TRIANGLES, 0, 36);
 
-    // Cube 4
-    GLM.mat4.rotateY(house.modelMatrices[3], house.modelMatrices[3], delta * -1);
-    gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[3] as Float32Array);
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
+    // // Cube 4
+    // GLM.mat4.rotateY(house.modelMatrices[3], house.modelMatrices[3], delta * -1);
+    // gl.uniformMatrix4fv(house.modelLoc, false, house.modelMatrices[3] as Float32Array);
+    // gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+    // Grid
+    bindVAO(grid.vao);
+    gl.uniformMatrix4fv(house.modelLoc, false, grid.modelMatrx as Float32Array);
+    gl.drawArrays(gl.LINES, 0, 2);
 
     // End frame
     gl.flush();
     gl.endFrameEXP();
     window.requestAnimationFrame(drawFrame);
+}
+
+function createVAO() {
+  // Ensure we have a WebGL context
+  if (!glRef) {
+    console.error("No gl context.");
+    return null;
+  }
+
+  if (!oesExt) {
+    // WebGL 2.0
+    return glRef.createVertexArray();
+  } else {
+    // WebGL 1.0
+    return oesExt.createVertexArrayOES();
+  }
+}
+
+function bindVAO(vao: WebGLVertexArrayObject | WebGLVertexArrayObjectOES | null) {
+  // Ensure we have a WebGL context
+  if (!glRef) {
+    console.error("No gl context.");
+    return null;
+  }
+
+  if (!oesExt) {
+    // WebGL 2.0
+    return glRef.bindVertexArray(vao);
+  } else {
+    // WebGL 1.0
+    return oesExt.bindVertexArrayOES(vao);
+  }
 }
 
 async function readShaderData() {
