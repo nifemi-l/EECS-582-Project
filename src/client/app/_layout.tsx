0f23fb9eefc736cfc54159c76db34f12c7e4e1dc
Jack Bauer <jgb1017@gmail.com>
2026-02-15 22:28:28 -0600

    Comment _layout.tsx

--- src/client/app/_layout.tsx
+++ src/client/app/_layout.tsx
@@ -1,5 +1,23 @@
+/* PROLOGUE
+File name: _layout.tsx
+Description: Define the basic root layout of the application using the expo router
+Programmer: Jack Bauer
+Creation date: 2/6/26
+Revision date: 
+  - 2/9/26: Disable header for new list view
+Preconditions: None
+Postconditions: None
+Errors: None
+Side effects: None
+Invariants: None
+Known faults: None
+*/
+
+// Import relevant components
 import { Stack } from "expo-router";
 
+// Export the root layout component as defined below.
+// Note we turn off showing the stack header
 export default function RootLayout() {
   return (
     <Stack
